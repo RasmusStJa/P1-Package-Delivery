716a48abd40d281fa1258316f39489a2547e5d94
RasmusStJa <PN83DC@student.aau.dk>
2024-12-05 09:07:06 +0100

    Worked further on implementing held-krap

--- main.c
+++ main.c
@@ -54,6 +54,16 @@ int min(const int a, const int b) {
     return b;
 }
 
+void* fillMatrix(const void* value, const int length1, const int length2) {
+    void* matrix[length1][length2];
+    for (int i = 0; i < length1; i++) {
+        for (int j = 0; j < length2; j++) {
+            matrix[i][j] = value;
+        }
+    }
+    return matrix;
+}
+
 void printAdjacencyMatrix(const adjacency_matrix *adjacency_matrix) {
     unsigned int length = sizeof(adjacency_matrix->nodes) / sizeof(adjacency_matrix->nodes[0]);
 
@@ -87,6 +97,7 @@ adjacency_matrix getFileContent(const char* fileName, char* fileContent[], char*
     char line[LINELENGTH];
     FILE *fptr = fopen(fileName, "r");
     adjacency_matrix matrix1;
+    matrix1.size = 0;
 
     if (fptr == NULL) {
         printf("File not found\n");
@@ -98,6 +109,28 @@ adjacency_matrix getFileContent(const char* fileName, char* fileContent[], char*
     while (fgets(line, LINELENGTH, fptr)) {  //while line is not empty (null)
         if (i == 0) {
             //if first time, get nodes (and amount)
+            const unsigned int line_size = strlen(line);
+            for (unsigned int j = 0; j < line_size; j++) {
+                const char current_char = line[j];
+                int last_space_index = 0;
+                bool end_loop = false;
+                switch (current_char) {
+                    case ' ':
+                        //this means that a new node's name starts, meaning more nodes, so increment matrix size
+                        if (j != line_size - 1) { //if not last
+                            matrix1.size++;
+                            last_space_index = j;
+                        }
+                        break;
+                    case '\n':
+                        end_loop = true;
+                        break;
+                    default:
+
+                }
+                if (end_loop) { break; }
+            }
+
             i++;
             continue;
         }
@@ -130,22 +163,30 @@ unsigned int getCost(const adjacency_matrix matrix, const node_g node1, const no
     return matrix.values[node1.index][node2.index];
 }
 
-void setup(const adjacency_matrix m, int *memory[][], const unsigned int S) {
+void setup(const adjacency_matrix m, int *memory[20][20], const node_g S) {
     const unsigned int N = m.size;
     for (unsigned int i = 0; i < N; i++) {
-        if (i == S) { continue; }
-        memory[i][0] = 0;
+        if (i == S.index) { continue; }  //skip the starting node
+        //cache optimal value from S.index to i
+        memory[i][1 << S.index | 1 << i] = m.values[S.index][i];
     }
 }
 
-void held_karp(const adjacency_matrix m, const unsigned int S, int *minCost, node_g* route[]) {
+void solve(const adjacency_matrix m, int *memory[20][20], const node_g S) {
+    const unsigned int N = m.size;
+    for (unsigned int r = 3; r <= N; r++) {
+
+    }
+}
+
+void held_karp(const adjacency_matrix m, const node_g S, int *minCost, node_g* route[]) {
     //TODO: implement code from video: https://youtu.be/cY4HiiFHO1o&t=576
     const unsigned int N = m.size;
 
     //initialize "memory" as +inf (INT_MAX is the closest to inf)
     //memory is list of indexes of visited nodes
-    int old_memory[pow(2, N)] = { INT_MAX };
-    int memory[N][pow(2,N)] = { old_memory };
+    int old_memory[(int)pow(2, N)] = {};
+    int memory[N][(int)pow(2,N)] = { old_memory };
 
     setup(m, memory, S);
     solve(m, memory, S);
